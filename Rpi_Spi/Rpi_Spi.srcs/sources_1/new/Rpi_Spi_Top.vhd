23f0b636677c78c714766b4788df853d83678857
LG92 <33595443+LuisGerman92@users.noreply.github.com>
2020-03-29 03:17:04 +0100

    Add files via upload

--- Rpi_Spi/Rpi_Spi.srcs/sources_1/new/Rpi_Spi_Top.vhd
+++ Rpi_Spi/Rpi_Spi.srcs/sources_1/new/Rpi_Spi_Top.vhd
@@ -23,105 +23,93 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Rpi_Spi_Top is
-  Port (
-    -- Clock in, SYS_CLK = 100 MHz
-    SYS_CLK : in std_logic;
-    
-    -- LED outs
-    led : out std_logic_vector(8 downto 1);
-    
-    RPI_CLK  : in  std_logic; -- SPI clock
-    RPI_MOSI : in  std_logic; -- SPI mosi
-    RPI_SS   : in  std_logic; -- SPI slave select (active low)
-    RPI_MISO : out std_logic  -- SPI miso
-     );
+	Port (
+		-- Clock in, CLK = 100 MHz
+		clk : in std_logic;
+		
+		-- 7 segment displays
+		seg	: out	std_logic_vector(7 downto 0);
+		an 	: out	std_logic_vector(3 downto 0);
+		
+		-- leds
+		led : out	std_logic_vector(7 downto 0);
+		
+		-- SPI Master Interface (Raspberry Pi)
+		RPI_CLK  : in  std_logic; -- SPI clock
+		RPI_MOSI : in  std_logic; -- SPI mosi
+		RPI_SS   : in  std_logic; -- SPI slave select (active low)
+		RPI_MISO : out std_logic  -- SPI miso
+	);
 end Rpi_Spi_Top;
 
 architecture arch of Rpi_Spi_Top is
-
--- Clock divider from 100 MHz to 29p5 MHz
-component clk_wiz_0 is
-    port(
-        clk_in1  : in  std_logic; -- input clock (100 MHz)
-        reset    : in  std_logic;
-        clk_out1 : out std_logic; -- Out clock (29p5 MHz)
-        locked   : out std_logic
-    );
-    end component;
+	-- Components    
+	component SPI_Slave is
+		port(
+			CLK      : in  std_logic; -- system clock
+			RST      : in  std_logic; -- high active synchronous reset
+			-- SPI SLAVE INTERFACE
+			SCLK     : in  std_logic; -- SPI clock
+			CS_N     : in  std_logic; -- SPI chip select, active in low
+			MOSI     : in  std_logic; -- SPI serial data from master to slave
+			MISO     : out std_logic; -- SPI serial data from slave to master
+			-- USER INTERFACE
+			DIN      : in  std_logic_vector(7 downto 0); -- input data for SPI master
+			DIN_VLD  : in  std_logic; -- when DIN_VLD = 1, input data are valid
+			READY    : out std_logic; -- when READY = 1, valid input data are accept
+			DOUT     : out std_logic_vector(7 downto 0); -- output data from SPI master
+			DOUT_VLD : out std_logic  -- when DOUT_VLD = 1, output data are valid
+		);
+	end component;
+	
+	component sseg_dec is
+    Port (      
+		ALU_VAL	: in	std_logic_vector(7 downto 0); 
+		SIGN	: in	std_logic;
+		VALID	: in	std_logic;
+		CLK		: in	std_logic;
+		DISP_EN	: out std_logic_vector(3 downto 0);
+		SEGMENTS: out std_logic_vector(7 downto 0)
+	);
+	end component;
     
--- SPI Slave
-component SPI_Slave is
-    port(
-      -- general purpose
-      sys_clock : in  std_logic; -- system clock
-      sys_rst   : in  std_logic; -- System reset (active low)
-      -- serial I/O side
-      sclk      : in  std_logic; -- SPI clock from Master
-      ss        : in  std_logic; -- SPI chip select from Master
-      mosi      : in  std_logic; -- SPI data from Master
-      miso      : out std_logic; -- SPI data to Master
-      -- Register read/write interface
-      wr_enable : out std_logic; -- Write Enable: Data_out is valid
-      data_out  : out std_logic_vector(7 downto 0); -- 8-bit data from Master
-      data_in   : in  std_logic_vector(7 downto 0)  -- 8-bit data read back
-    );
-    end component;
-    
--- Signals
-signal locked, clk_29MHz_i, rst_i : std_logic := '0';
-signal data_out, SPI_DATA         : std_logic_vector(7 downto 0);
-    
-signal rspi_clk  : std_logic;
-signal rspi_mosi : std_logic;
-signal rspi_miso : std_logic;
-signal rspi_ss   : std_logic;
-    
-signal wr_enable : std_logic;
+	-- Signals
+	signal spi_data_out			:	std_logic_vector(7 downto 0);
+	signal spi_data_out_valid	:	std_logic;
+	-- unused signals
+	signal din			: std_logic_vector(7 downto 0) := (others => '0');
+	signal din_valid	: std_logic := '0';
+	signal ready		: std_logic := '0';
+	
 
 begin
--- port maps
-clk_pm : clk_wiz_0
-    port map (
-        clk_in1  => SYS_CLK,
-        reset    => '0',
-        clk_out1 => clk_29MHz_i,
-        locked   => locked
-    );
-    
-spi_slave_pm : SPI_Slave
-    port map (
-        -- general purpose
-        sys_clock => clk_29MHz_i,
-        sys_rst   => rst_i,
-        -- serial I/O side
-        sclk      => rspi_clk,
-        ss        => rspi_ss,
-        mosi      => rspi_mosi,
-        miso      => rspi_miso,
-        -- Register read/write interface
-        wr_enable => wr_enable,
-        data_out  => data_out,
-        data_in   => SPI_DATA
-    );
-    
--- Spi data register process
-spi_data_p : process(clk_29MHz_i, rst_i)
-begin
-    if(rst_i = '0') then
-        SPI_DATA <= (others => '0');
-    elsif(rising_edge(clk_29MHz_i)) then
-        if (wr_enable = '1') then
-            SPI_DATA <= data_out;
-        end if;
-    end if;
-end process;
+	-- port maps
+	spi_slave_pm : SPI_Slave
+		port map (
+			CLK			=> CLK,
+			RST			=> '1',
+			-- SPI SLAVE INTERFACE
+			SCLK		=> RPI_CLK,
+			CS_N		=> RPI_SS,
+			MOSI		=> RPI_MOSI,
+			MISO		=> RPI_MISO,
+			-- USER INTERFACE
+			DIN			=> din,
+			DIN_VLD		=> din_valid,
+			READY		=> ready,
+			DOUT		=> spi_data_out,
+			DOUT_VLD	=> spi_data_out_valid
+		);
+		
+	sseg_dec_pm : sseg_dec
+		port map (
+			ALU_VAL		=> spi_data_out,
+			SIGN		=> '0',
+			VALID		=> spi_data_out_valid,
+			CLK			=> CLK,
+			DISP_EN		=> an,
+			SEGMENTS	=> seg
+		);
 
-rspi_clk    <= RPI_CLK;
-rspi_mosi   <= RPI_MOSI;
-rspi_ss     <= RPI_SS;
-RPI_MISO    <= rspi_miso;
-
-led  <= not SPI_DATA;
-
-rst_i <= locked;
+	led <= spi_data_out;
 end arch;
--- Rpi_Spi/Rpi_Spi.srcs/sources_1/new/SPI_Slave.vhd
+++ Rpi_Spi/Rpi_Spi.srcs/sources_1/new/SPI_Slave.vhd
@@ -1,146 +1,214 @@
-----------------------------------------------------------------------------------
--- Create Date: 23.02.2020 13:27:42
--- Module Name: SPI_Slave - Behavioral
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- PROJECT: SPI MASTER AND SLAVE FOR FPGA
+--------------------------------------------------------------------------------
+-- NAME:    SPI_SLAVE
+-- AUTHORS: Jakub Cabal <jakubcabal@gmail.com>
+-- LICENSE: LGPL-3.0, please read LICENSE file
+-- WEBSITE: https://github.com/jakubcabal/spi-fpga
+--------------------------------------------------------------------------------
+-- COPYRIGHT NOTICE:
+--------------------------------------------------------------------------------
+-- SPI MASTER AND SLAVE FOR FPGA
+-- Copyright (C) 2016 Jakub Cabal
+--
+-- This source file is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Lesser General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This source file is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Lesser General Public License for more details.
+--
+-- You should have received a copy of the GNU Lesser General Public License
+-- along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
-entity SPI_Slave is
-  Port (
-  -- general purpose
-  sys_clock : in  std_logic; -- system clock
-  sys_rst   : in  std_logic; -- System reset (active low)
-  -- serial I/O side
-  sclk      : in  std_logic; -- SPI clock from Master
-  ss        : in  std_logic; -- SPI chip select from Master
-  mosi      : in  std_logic; -- SPI data from Master
-  miso      : out std_logic; -- SPI data to Master
-  -- Register read/write interface
-  wr_enable : out std_logic; -- Write Enable: Data_out is valid
-  data_out  : out std_logic_vector(7 downto 0); -- 8-bit data from Master
-  data_in   : in  std_logic_vector(7 downto 0)  -- 8-bit data read back
-  );
-end SPI_Slave;
+-- THE SPI SLAVE MODULE SUPPORT ONLY SPI MODE 0 (CPOL=0, CPHA=0)!!!
 
-architecture Behavioral of SPI_Slave is
+entity SPI_SLAVE is
+    Port (
+        CLK      : in  std_logic; -- system clock
+        RST      : in  std_logic; -- high active synchronous reset
+        -- SPI SLAVE INTERFACE
+        SCLK     : in  std_logic; -- SPI clock
+        CS_N     : in  std_logic; -- SPI chip select, active in low
+        MOSI     : in  std_logic; -- SPI serial data from master to slave
+        MISO     : out std_logic; -- SPI serial data from slave to master
+        -- USER INTERFACE
+        DIN      : in  std_logic_vector(7 downto 0); -- input data for SPI master
+        DIN_VLD  : in  std_logic; -- when DIN_VLD = 1, input data are valid
+        READY    : out std_logic; -- when READY = 1, valid input data are accept
+        DOUT     : out std_logic_vector(7 downto 0); -- output data from SPI master
+        DOUT_VLD : out std_logic  -- when DOUT_VLD = 1, output data are valid
+    );
+end SPI_SLAVE;
 
-    signal spi_clk_rising_edge  : std_logic;
-    signal spi_clk_falling_edge : std_logic;
-    signal spi_write_cycle      : std_logic;
-    signal spi_clk_dly_line     : std_logic_vector(2 downto 0);
-    signal spi_clk_count        : unsigned(5 downto 0); -- count # of clk cycles
-    signal spi_datain_shifter   : std_logic_vector(7 downto 0);
-    signal spi_dataout_shifter  : std_logic_vector(7 downto 0);
+architecture RTL of SPI_SLAVE is
 
-begin    
--- edge detection process
--- This method requires that sys_clk be at least 4 times faster than spi clock
-  spi_clk_edge_detect_p : process (sys_clock)
-  begin
-    if(rising_edge(sys_clock)) then
-        -- delay spi_clk three system clock cycles
-        spi_clk_dly_line <= spi_clk_dly_line(1 downto 0) & sclk;
-        
-        -- rising edge detected when two clock cycles low and then two clock cycles high
-        if(spi_clk_dly_line(2) = '0' and 
-           spi_clk_dly_line(1) = '0' and
-           spi_clk_dly_line(0) = '1' and
-           sclk = '1') then
-                spi_clk_rising_edge <= '1';
-        else
-                spi_clk_rising_edge <= '0';
+    signal spi_clk_reg        : std_logic;
+    signal spi_clk_redge_en   : std_logic;
+    signal spi_clk_fedge_en   : std_logic;
+    signal bit_cnt            : unsigned(2 downto 0);
+    signal bit_cnt_max        : std_logic;
+    signal last_bit_en        : std_logic;
+    signal load_data_en       : std_logic;
+    signal data_shreg         : std_logic_vector(7 downto 0);
+    signal slave_ready        : std_logic;
+    signal shreg_busy         : std_logic;
+    signal rx_data_vld        : std_logic;
+
+begin
+
+    -- -------------------------------------------------------------------------
+    --  SPI CLOCK REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- The SPI clock register is necessary for clock edge detection.
+    spi_clk_reg_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1') then
+                spi_clk_reg <= '0';
+            else
+                spi_clk_reg <= SCLK;
+            end if;
         end if;
-        -- falling edge detected when two clock cycles high and then two clock cycles low
-        if(spi_clk_dly_line(2) = '1' and 
-           spi_clk_dly_line(1) = '1' and
-           spi_clk_dly_line(0) = '0' and
-           sclk = '0') then
-               spi_clk_rising_edge <= '1';
-        else
-               spi_clk_rising_edge <= '0';
-        end if;
-    end if;
-  end process;
-  
-  spi_clock_cycles_counter_p : process(sys_clock)
-  begin
-    if(rising_edge(sys_clock)) then
-      if(ss = '1') then -- reset counter to 0 when not chip select
-        spi_clk_count <= (others => '0');
-      else
-        if (spi_clk_rising_edge = '1') then -- every rising edge add one
-          if (spi_clk_count = 16) then
-            spi_clk_count <= (others => '0');
-          else
-            spi_clk_count <= spi_clk_count + 1;
-          end if;
-        end if;
-      end if;
-    end if;
-  end process;
-  
-  -- Every time SPI chip select goes to low, it will have 16 bit data
-  spi_in_p : process (sys_clock)
-  begin
-    if (rising_edge(sys_clock)) then
-      if (spi_clk_rising_edge = '1') then
-        spi_datain_shifter <= spi_datain_shifter(6 downto 0) & mosi;
-      end if;
-    end if;
-  end process;
-  
-  read_write_cycle_p : process (sys_clock, sys_rst)
-  begin
-    if (sys_rst = '0') then
-      spi_write_cycle <= '0';
-    elsif (rising_edge(sys_clock)) then
-      if (ss = '1') then
-        spi_write_cycle <= '0';
-      elsif (spi_clk_count = 8 and spi_datain_shifter = x"00" and spi_clk_falling_edge = '1') then
-        spi_write_cycle <= '1';
-      end if;
-    end if;
-  end process;
-  
-  write_input_p : process(sys_clock, sys_rst)
-  begin
-    if (sys_rst = '0') then
-      wr_enable <= '0';
-      data_out <= (others => '0');
-    elsif (rising_edge(sys_clock)) then
-      if (ss = '1') then
-        wr_enable <= '0';
-      elsif (spi_clk_count = 16 and spi_write_cycle = '1' and spi_clk_falling_edge = '1') then
-        wr_enable <= '1';
-        data_out <= spi_datain_shifter;
-      else
-        wr_enable <= '0';
-      end if;
-    end if;
-  end process;
-  
-  read_output_p : process(sys_clock, sys_rst)
-  begin
-  if(sys_rst = '0') then
-    spi_dataout_shifter <= (others => '0');
-  elsif(rising_edge(sys_clock))then
-    if(spi_clk_count = 8 and spi_datain_shifter = x"01" and spi_clk_falling_edge = '1')
-  then
-    spi_dataout_shifter <= data_in;
-  elsif(spi_clk_falling_edge = '1') then
-    spi_dataout_shifter <= spi_dataout_shifter(6 downto 0)& '0';
-  end if;
-  end if;
-  end process;
-  
-  miso <= spi_dataout_shifter(7); -- msb send to the SPI master
-end Behavioral;
+    end process;
 
---=============================================================================
--- architecture end
---=============================================================================
+    -- -------------------------------------------------------------------------
+    --  SPI CLOCK EDGES FLAGS
+    -- -------------------------------------------------------------------------
+
+    -- Falling edge is detect when SCLK=0 and spi_clk_reg=1.
+    spi_clk_fedge_en <= not SCLK and spi_clk_reg;
+    -- Rising edge is detect when SCLK=1 and spi_clk_reg=0.
+    spi_clk_redge_en <= SCLK and not spi_clk_reg;
+
+    -- -------------------------------------------------------------------------
+    --  RECEIVED BITS COUNTER
+    -- -------------------------------------------------------------------------
+
+    -- The counter counts received bits from the master. Counter is enabled when
+    -- falling edge of SPI clock is detected and not asserted CS_N.
+    bit_cnt_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1') then
+                bit_cnt <= (others => '0');
+            elsif (spi_clk_fedge_en = '1' and CS_N = '0') then
+                if (bit_cnt_max = '1') then
+                    bit_cnt <= (others => '0');
+                else
+                    bit_cnt <= bit_cnt + 1;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    -- The flag of maximal value of the bit counter.
+    bit_cnt_max <= '1' when (bit_cnt = "111") else '0';
+
+    -- -------------------------------------------------------------------------
+    --  LAST BIT FLAG REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- The flag of last bit of received byte is only registered the flag of
+    -- maximal value of the bit counter.
+    last_bit_en_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1') then
+                last_bit_en <= '0';
+            else
+                last_bit_en <= bit_cnt_max;
+            end if;
+        end if;
+    end process;
+
+    -- -------------------------------------------------------------------------
+    --  RECEIVED DATA VALID FLAG
+    -- -------------------------------------------------------------------------
+
+    -- Received data from master are valid when falling edge of SPI clock is
+    -- detected and the last bit of received byte is detected.
+    rx_data_vld <= spi_clk_fedge_en and last_bit_en;
+
+    -- -------------------------------------------------------------------------
+    --  SHIFT REGISTER BUSY FLAG REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- Data shift register is busy until it sends all input data to SPI master.
+    shreg_busy_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (RST = '1') then
+                shreg_busy <= '0';
+            else
+                if (DIN_VLD = '1' and (CS_N = '1' or rx_data_vld = '1')) then
+                    shreg_busy <= '1';
+                elsif (rx_data_vld = '1') then
+                    shreg_busy <= '0';
+                else
+                    shreg_busy <= shreg_busy;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    -- The SPI slave is ready for accept new input data when CS_N is assert and
+    -- shift register not busy or when received data are valid.
+    slave_ready <= (CS_N and not shreg_busy) or rx_data_vld;
+    
+    -- The new input data is loaded into the shift register when the SPI slave
+    -- is ready and input data are valid.
+    load_data_en <= slave_ready and DIN_VLD;
+
+    -- -------------------------------------------------------------------------
+    --  DATA SHIFT REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- The shift register holds data for sending to master, capture and store
+    -- incoming data from master.
+    data_shreg_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (load_data_en = '1') then
+                data_shreg <= DIN;
+            elsif (spi_clk_redge_en = '1' and CS_N = '0') then
+                data_shreg <= data_shreg(6 downto 0) & MOSI;
+            end if;
+        end if;
+    end process;
+
+    -- -------------------------------------------------------------------------
+    --  MISO REGISTER
+    -- -------------------------------------------------------------------------
+
+    -- The output MISO register ensures that the bits are transmit to the master
+    -- when is not assert CS_N and falling edge of SPI clock is detected.
+    miso_p : process (CLK)
+    begin
+        if (rising_edge(CLK)) then
+            if (load_data_en = '1') then
+                MISO <= DIN(7);
+            elsif (spi_clk_fedge_en = '1' and CS_N = '0') then
+                MISO <= data_shreg(7);
+            end if;
+        end if;
+    end process;
+
+    -- -------------------------------------------------------------------------
+    --  ASSIGNING OUTPUT SIGNALS
+    -- -------------------------------------------------------------------------
+    
+    READY    <= slave_ready;
+    DOUT     <= data_shreg;
+    DOUT_VLD <= rx_data_vld;
+
+end RTL;
