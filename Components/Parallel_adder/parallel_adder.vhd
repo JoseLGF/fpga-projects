9834125b2e0290cf1871f6bdbe665a29a6426841
Jose Luis German <luisgermanfelix@gmail.com>
2020-04-01 21:59:34 -0600

    Add generic N-bit parallel adder with test bench.

---
+++ Components/Parallel_adder/parallel_adder.vhd
@@ -0,0 +1,55 @@
+-------------------------------------------------------------------------------
+--
+-- Component name: Generic N-bit parallel adder
+--
+-------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity parallel_adder is
+	generic (
+		-- n must be >= 2 for this component to synthesize properly
+		n	: integer := 4
+	);
+	port (
+		A		: in  std_logic_vector(n-1 downto 0);
+		B		: in  std_logic_vector(n-1 downto 0);
+		Sum		: out std_logic_vector(n   downto 0)
+	);
+end entity;
+
+architecture gen of parallel_adder is
+
+	component full_adder is
+	port(
+		a    : in  std_logic;
+		b    : in  std_logic;
+		cin  : in  std_logic;
+		sum  : out std_logic;
+		cout : out std_logic
+	);
+	end component;
+	
+	--signals
+	signal carries : std_logic_vector(n+1 downto 0) := (others => '0');
+	
+begin
+
+	GEN_REG: 
+	for I in 0 to n-1 generate
+		-- full adder generic port mapping
+		fa_gpm : full_adder port map (
+			a		=> A(I),
+			b		=> B(I),
+			cin		=> carries(I),
+			sum		=> Sum(I),
+			cout	=> carries(I+1)
+		);
+   end generate GEN_REG;
+   
+   -- Last carry is Sum's MSB
+   Sum(n) <= carries(n);
+   
+end gen;
---
+++ Components/Parallel_adder/parallel_adder_tb.vhd
@@ -0,0 +1,65 @@
+----------------------------------------------------------------------------------
+--
+-- Module Name: parallel adder testbench
+-- Description:
+-- A simulation to test the proper behavior of parallel adder component.
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use ieee.std_logic_arith.all;
+USE ieee.numeric_std.ALL;
+
+entity parallel_adder_tb is
+end parallel_adder_tb;
+
+architecture test of parallel_adder_tb is
+	-- constant declaration for generics instantiating
+	constant N : integer := 4;
+
+    -- components
+    component parallel_adder
+        generic (
+		-- n must be >= 2 for this component to synthesize properly
+		n	: integer := 4
+		);
+		port (
+			A		: in  std_logic_vector(n-1 downto 0);
+			B		: in  std_logic_vector(n-1 downto 0);
+			Sum		: out std_logic_vector(n   downto 0)
+		);
+    end component;
+    
+	
+	signal A		: std_logic_vector	(N-1 downto 0) := "0000";
+	signal B		: std_logic_vector	(N-1 downto 0) := "0000";
+	signal Sum		: std_logic_vector	(N   downto 0) := "00000";
+	
+
+begin
+
+    parallel_adder_pm: parallel_adder
+	generic map (
+		n => N
+	)
+	
+	port map(
+		A	=> A,
+		B	=> B,
+		Sum	=> Sum
+	);
+	
+	-- Adder values iteration
+	process begin
+		wait for 10 ns;
+		A <= std_logic_vector( unsigned(A) + 1 );
+	end process;
+	
+	process begin
+		wait for 160 ns;
+		B <= STD_LOGIC_VECTOR ( unsigned(B) + 1);
+	end process;
+	
+end test;
