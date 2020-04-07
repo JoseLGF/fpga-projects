12ad975a0135073ccdfb703a11395c00abd216f8
Jose Luis German <luisgermanfelix@gmail.com>
2020-04-07 00:58:17 -0500

    Fix project path that was causing Jenkins to break.

--- Raspberry_spi/build.bat
+++ Raspberry_spi/build.bat
@@ -3,10 +3,10 @@
 call C:/Xilinx/Vivado/2018.2/settings64.bat
 
 echo Generating project
-call vivado -mode batch -source build.tcl
+call vivado -mode batch -source build.tcl || exit /B 1
 
 echo compile, synthesize, implement, and generate bitstream
-call vivado -mode batch -source synth_imp_bitstr.tcl
+call vivado -mode batch -source synth_imp_bitstr.tcl || exit /B 1
 
 echo Generation completed
 copy ".\work\Raspberry_spi\Raspberry_spi.runs\impl_1\Rpi_Spi_Top.bit" ".\work\bitstream\Rpi_Spi_Top.bit"
--- Raspberry_spi/build.tcl
+++ Raspberry_spi/build.tcl
@@ -102,7 +102,7 @@ if { $::argc > 0 } {
 set orig_proj_dir "[file normalize "$origin_dir/../work/Raspberry_spi"]"
 
 # Create project
-create_project ${_xil_proj_name_} ./work/${_xil_proj_name_} -part xc7a35tcpg236-1
+create_project Raspberry_spi ./work/Raspberry_spi -part xc7a35tcpg236-1
 
 # Set the directory path for the new project
 set proj_dir [get_property directory [current_project]]
