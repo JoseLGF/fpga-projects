bab13bf991fb14dc5cb8fa956f219124fed1a655
Jose Luis German <luisgermanfelix@gmail.com>
2020-04-07 13:52:15 -0500

    Split project generation phase and synthesis into two separate scripts.

--- Raspberry_spi/synth_implement.bat
+++ Raspberry_spi/synth_implement.bat
@@ -4,3 +4,5 @@ call C:/Xilinx/Vivado/2018.2/settings64.bat
 
 echo compile, synthesize, implement, and generate bitstream
 call vivado -mode batch -source synth_imp_bitstr.tcl || exit /B 1
+
+echo Generation completed
